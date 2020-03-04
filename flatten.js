d80bba8b9c838de359544a9ceffaf12e29058ef6
Alan Marx <alanmarx@hotmail.com>
2020-03-04 20:55:08 +0000

    flatten takes an array with nested arrays and produces just one aray

---
+++ flatten.js
@@ -0,0 +1,45 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArrayEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`😇Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`😡Assertion Failed: ${JSON.stringify(firstArray)} !== ${JSON.stringify(secondArray)}`);
+  }
+};
+
+//flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+
+
+
+
+const flatten = function(array) {
+  let flatArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let i2 = 0; i2 < array[i].length; i2++) {
+      flatArray.push(array[i][i2]);
+      }
+    } else {
+      flatArray.push(array[i]);
+    };
+  }
+  return flatArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6, 8, 9]]));
+assertArrayEqual((flatten([1, 2, [3, 4], 5, [6, 8, 9]])), [1, 2, 3, 4, 5, 6, 8, 9]);
+assertArrayEqual((flatten([])), []);
+assertArrayEqual((flatten([[1, 2, 3], 3, 4])), [1, 2, 3]);
+//array.isArray(variableName) -> returns a boolean value.
