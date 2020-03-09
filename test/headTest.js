2b1af145fa33087a20ce0889275ebd7f1af732df
Alan Marx <alanmarx@hotmail.com>
2020-03-09 21:00:09 +0000

    added mocha and chai to headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,34 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+  assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([9]), 78);
-assertEqual(head([]), undefined); 
+  it('returns \'5\' for [\'5\']', () => {
+    assert.strictEqual(head(['5']), '5');
+    });
+
+  it(`returns 'Hello' for ["Hello", "Lighthouse", "Labs"]`, () => {
+    const input = ["Hello", "Lighthouse", "Labs"];
+    const result = head(input);
+    assert.strictEqual(result, 'Hello');
+    });
+
+  it(`returns 1 for [1]`, () => {
+    const input = [1];
+    const result = head(input);
+    assert.strictEqual(result, 1);
+    });   
+
+
+  it(`returns undefined for []]`, () => {
+    const input = [];
+    const result = head(input);
+    assert.strictEqual(result, undefined);
+    });      
+});
+
+
+
