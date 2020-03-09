abe3753fa53f6b151dd99d51cf2773f5fbb6b29b
Alan Marx <alanmarx@hotmail.com>
2020-03-09 16:54:12 +0000

    refactored assertArraysEqual and created a test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,39 +1,16 @@
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😇Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // this function is designed to take values, and print that the assertion has passed if the
 // values are identical, or print that the assertion has failed if they are not. Works best with boolean values
 
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  } else {
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray[i] !== secondArray[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArrayEqual = function(firstArray, secondArray) {
   if (eqArrays(firstArray, secondArray)) {
-    console.log(`😇Assertion Passed: ${firstArray} === ${secondArray}`);
+    console.log(`😇Assertion Passed: ${JSON.stringify(firstArray)} === ${JSON.stringify(secondArray)}`);
   } else {
     console.log(`😡Assertion Failed: ${JSON.stringify(firstArray)} !== ${JSON.stringify(secondArray)}`);
   }
 };
 
-assertArrayEqual(['Lighthouse Labs', 'Fast'], ['Bootcamp']);
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArrayEqual;
 
-
-  
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArrayEqual = require('../assertArraysEqual');
+
+assertArrayEqual(['Lighthouse Labs', 'Fast'], ['Bootcamp']);
+assertArrayEqual([1, 2, 3], [1, 2, 3]);
+assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
+
+
+  
